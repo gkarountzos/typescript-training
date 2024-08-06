3c535a858a81610e3fbb75e2fc34511617c2f55f
George Karountzos <george3-2001@hotmail.com>
2024-08-06 10:22:55 +0300

    added classes/constructors/interfaces

--- basics/app.js
+++ basics/app.js
@@ -1,18 +1,75 @@
+// class User {
+//   name: string;
+//   private age: number;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var User = /** @class */ (function () {
+    function User(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    User.prototype.print = function () {
+        console.log(this.name);
+    };
+    return User;
+}());
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin(name, age, permissions) {
+        var _this = _super.call(this, name, age) || this;
+        _this.permissions = permissions;
+        return _this;
+    }
+    return Admin;
+}(User));
+var user = new User("Max", 30);
+console.log(user.name);
 var numOneInput = document.getElementById("num1");
 var numTwoInput = document.getElementById("num2");
 var buttonEl = document.querySelector("button");
 function add(a, b) {
     return a + b;
 }
-function printResult(result) {
-    console.log(result);
+var OutputMode;
+(function (OutputMode) {
+    OutputMode[OutputMode["CONSOLE"] = 0] = "CONSOLE";
+    OutputMode[OutputMode["ALERT"] = 1] = "ALERT";
+})(OutputMode || (OutputMode = {}));
+function printResult(result, printMode) {
+    if (printMode === OutputMode.CONSOLE) {
+        console.log(result);
+    }
+    else if (printMode === OutputMode.ALERT) {
+        alert(result);
+    }
 }
-// const result = add(5, 3);
-// let isDone = false;
-// console.log(result);
+var results = [];
+var names = ["George"];
 buttonEl === null || buttonEl === void 0 ? void 0 : buttonEl.addEventListener("click", function () {
     var num1 = +numOneInput.value;
     var num2 = +numTwoInput.value;
     var result = add(num1, num2);
-    printResult(result);
+    var resultContainer = {
+        res: result,
+        print: function () {
+            console.log(this.res);
+        },
+    };
+    results.push(resultContainer);
+    // results[0].print;
+    printResult(result, OutputMode.CONSOLE);
+    printResult(result, OutputMode.ALERT);
 });
--- basics/app.ts
+++ basics/app.ts
@@ -1,3 +1,39 @@
+// class User {
+//   name: string;
+//   private age: number;
+
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// const user = new User("Max", 30);
+// console.log(user.name);
+interface Greetable {
+  name: string;
+}
+
+interface Printable {
+  print(): void;
+}
+class User implements Greetable, Printable {
+  constructor(public name: string, private age: number) {}
+
+  print() {
+    console.log(this.name);
+  }
+}
+
+class Admin extends User {
+  constructor(name: string, age: number, private permissions: string[]) {
+    super(name, age);
+  }
+}
+
+const user = new User("Max", 30);
+console.log(user.name);
+
 const numOneInput = document.getElementById("num1") as HTMLInputElement;
 const numTwoInput = document.getElementById("num2") as HTMLInputElement;
 const buttonEl = document.querySelector("button");
@@ -6,8 +42,19 @@ function add(a: number, b: number) {
   return a + b;
 }
 
-function printResult(result: number) {
-  console.log(result);
+type PrintMode = "console" | "alert";
+
+enum OutputMode {
+  CONSOLE,
+  ALERT,
+}
+
+function printResult(result: string | number, printMode: OutputMode) {
+  if (printMode === OutputMode.CONSOLE) {
+    console.log(result);
+  } else if (printMode === OutputMode.ALERT) {
+    alert(result);
+  }
 }
 
 // const result = add(5, 3);
@@ -15,12 +62,28 @@ function printResult(result: number) {
 
 // console.log(result);
 
+interface CalculationContainer {
+  res: number;
+  print(): void;
+}
+
+type CalculationResults = CalculationContainer[]; //custom type - alias
+
+let results: CalculationResults = [];
+const names = ["George"];
+
 buttonEl?.addEventListener("click", () => {
   const num1 = +numOneInput.value;
   const num2 = +numTwoInput.value;
   const result = add(num1, num2);
   const resultContainer = {
     res: result,
+    print() {
+      console.log(this.res);
+    },
   };
-  printResult(result);
+  results.push(resultContainer);
+  // results[0].print;
+  printResult(result, OutputMode.CONSOLE);
+  printResult(result, OutputMode.ALERT);
 });
